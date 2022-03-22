2ad4558a7cd248b6f6dd7d7974d6a3f7ed360b7d
yshalsager <ysh-alsager@hotmail.com>
2022-03-22 16:50:22 +0200

    fix(start): Escape user name markdown
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- droos_bot/modules/start.py
+++ droos_bot/modules/start.py
@@ -10,7 +10,7 @@ from droos_bot.utils.keyboards import main_keyboard
 def start_handler(update: Update, _: CallbackContext) -> None:
     assert update.effective_user is not None
     welcome_text = (
-        f" مرحبا بك يا {update.effective_user.full_name}"
+        f" مرحبا بك يا `{update.effective_user.full_name}`"
         f"\n بإمكانك استخدام البوت من خلال الضغط على الأزرار الظاهرة بالأسفل"
     )
     update.message.reply_text(welcome_text, reply_markup=main_keyboard)
