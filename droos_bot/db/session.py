f431fe4e87ad33ac19aba7ba2d2888da667885b4
yshalsager <ysh-alsager@hotmail.com>
2022-03-23 12:06:51 +0200

    fix(db): Use scoped session to prevent invalid requests
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- droos_bot/db/session.py
+++ droos_bot/db/session.py
@@ -1,6 +1,6 @@
 """Database initialization"""
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from droos_bot import PARENT_DIR
 from droos_bot.db.base import Base
@@ -10,5 +10,4 @@ engine = create_engine(db_connection_string, connect_args={"check_same_thread": 
 
 Base.metadata.create_all(bind=engine)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = SessionLocal()
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
