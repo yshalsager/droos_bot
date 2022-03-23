9ee7fc53487ab9ac603aff23196db6a30c2af1cf
yshalsager <ysh-alsager@hotmail.com>
2022-03-23 21:44:41 +0200

    refactor(feedback): Re-write reply_to_feedback message handing and use HTML
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- droos_bot/modules/feedback_and_files.py
+++ droos_bot/modules/feedback_and_files.py
@@ -3,7 +3,7 @@ Feedback and files handler module.
 """
 import re
 
-from telegram import Update
+from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
     CommandHandler,
@@ -69,31 +69,40 @@ def files_handler(update: Update, _: CallbackContext) -> int:
 def reply_to_feedback(update: Update, context: CallbackContext) -> None:
     assert update.effective_message is not None
     assert context.match is not None
+    # Handle the update if the message replied to is forwarded from the bot itself only
+    if not update.effective_message.reply_to_message.from_user.id == context.bot.id:
+        return
     if not update.effective_message.reply_to_message.forward_from:
         update.effective_message.reply_text(
             "لا يمكن الرد على هذا المستخدم بسبب إعدادات حسابه",
             reply_to_message_id=update.effective_message.message_id,
         )
         return
-    replied_to_message_text = (
-        update.effective_message.reply_to_message.text_markdown_v2_urled
-    )
+    replied_to_message_text = update.effective_message.reply_to_message.text_html_urled
+    admin_reply_message_text = update.effective_message.text_html_urled.replace(
+        context.match.group(1), ""
+    ).strip()
     reply_with_message_text = (
-        f"*رسالتك:*\n```\n{replied_to_message_text}\n```\n\n"
-        f"*رد المشرف*:\n{context.match.group(1)}\n\n"
-        f"للرد على هذه الرسالة اضغط على زر التواصل والاقتراحات أولا ثم أرسل الرد"
+        f"<b>رسالتك:</b>\n{replied_to_message_text}\n\n"
+        f"<b>رد المشرف</b>:\n{admin_reply_message_text}\n\n"
+        f"<b>ملاحظة</b>:\nللرد على هذه الرسالة اضغط على زر التواصل والاقتراحات أولا ثم أرسل الرد"
     )
+    # TODO handle a case when message text is >= telegram.MAX_MESSAGE_LENGTH
     context.bot.send_message(
         update.effective_message.reply_to_message.forward_from.id,
         reply_with_message_text,
+        parse_mode=ParseMode.HTML,
+    )
+    admin_message = (
+        f'<a href="{update.effective_message.reply_to_message.link}">رُد</a> على '
+        f'<a href="tg://user?id={update.effective_message.reply_to_message.forward_from.id}">'
+        f"{update.effective_message.reply_to_message.forward_from.full_name}</a> بـ:\n"
+        f"{admin_reply_message_text}"
     )
     update.effective_message.reply_text(
-        f"[رُد]({update.effective_message.reply_to_message.link})"
-        f" على [{update.effective_message.reply_to_message.forward_from.full_name}]"
-        f"(tg://user?id={update.effective_message.reply_to_message.forward_from.id})"
-        f" بـ:\n"
-        f"```\n{context.match.group(1)}\n```",
+        admin_message,
         reply_to_message_id=update.effective_message.message_id,
+        parse_mode=ParseMode.HTML,
     )
 
 
@@ -142,7 +151,7 @@ dispatcher.add_handler(files_conversation_handler)
 filter_feedback_message = FeedbackMessageFilter(CONFIG["tg_feedback_chat_id"])
 dispatcher.add_handler(
     MessageHandler(
-        Filters.regex(re.compile(r"^(?:/reply|/r)\s+([\S\s]+)$", re.M))
+        Filters.regex(re.compile(r"^(/reply|/r)\s+([\S\s]+)$", re.M))
         & filter_feedback_message,
         reply_to_feedback,
     )
