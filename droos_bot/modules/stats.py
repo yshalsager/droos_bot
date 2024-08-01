5161e62d25ddedc1c6b0747a3fc80495cdcbddc0
yshalsager <ysh-alsager@hotmail.com>
2024-08-01 15:37:26 +0300

    chore(stats): use str.format and html
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- droos_bot/modules/stats.py
+++ droos_bot/modules/stats.py
@@ -24,21 +24,21 @@ async def stats(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     top_lectures: list[Lecture] = get_top_lectures()
 
     message = (
-        f"**المستخدمون الحاليون**: {active_chats!s}\n"
-        f"**كل المستخدمون**: {all_chats!s}\n"
-        f"**إجمالي مرات الاستخدام**: {usage_times!s}\n"
-        f"**إجمالي الملفات المرسلة**: {series_requests!s}\n"
-        f"**إجمالي الدروس المطلوبة**: {lecture_requests!s}\n\n"
-        f"**أكثر السلاسل طلبًا**:\n"
-        f"$top_series\n"
-        f"**أكثر الدروس طلبًا**:\n"
-        f"$top_lectures\n"
+        f"<b>المستخدمون الحاليون</b>: {active_chats!s}\n"
+        f"<b>كل المستخدمون</b>: {all_chats!s}\n"
+        f"<b>إجمالي مرات الاستخدام</b>: {usage_times!s}\n"
+        f"<b>إجمالي الملفات المرسلة</b>: {series_requests!s}\n"
+        f"<b>إجمالي الدروس المطلوبة</b>: {lecture_requests!s}\n\n"
+        f"<b>أكثر السلاسل طلبًا</b>:\n"
+        f"{{top_series_placeholder}}\n"
+        f"<b>أكثر الدروس طلبًا</b>:\n"
+        f"{{top_lectures_placeholder}}\n"
     )
     top_series_message = ""
     if top_series:
         for series in top_series:
             try:
-                top_series_message += f"  `{sheet.df[sheet.df.series_slug == series.id].iloc[0].series}`: {series.requests!s} مرة\n"
+                top_series_message += f" • {sheet.df[sheet.df.series_slug == series.id].iloc[0].series}: {series.requests!s} مرة\n"
             except IndexError:
                 continue
     top_lectures_message = ""
@@ -46,14 +46,14 @@ async def stats(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
         for lecture in top_lectures:
             try:
                 lecture_info: DataFrame = sheet.df[sheet.df.id == lecture.id]
-                top_lectures_message += f"  `{lecture_info.series.item()} ({lecture_info.lecture.item()})`: {lecture.requests!s} مرة\n"
+                top_lectures_message += f" • {lecture_info.series.item()} ({lecture_info.lecture.item()}): {lecture.requests!s} مرة\n"
             except ValueError:
                 continue
-    message = message.replace("$top_series", top_series_message).replace(
-        "$top_lectures", top_lectures_message
+    message = message.format(
+        top_series_placeholder=top_series_message, top_lectures_placeholder=top_lectures_message
     )
 
-    await stats_message.edit_text(message, parse_mode=ParseMode.MARKDOWN_V2)
+    await stats_message.edit_text(message, parse_mode=ParseMode.HTML)
 
 
 application.add_handler(CommandHandler("stats", stats, FilterBotAdmin()))
